b992116931696eec77531da67f73ad18d7ad2f1a
Eleazar Curiel Monjaraz <eleazar.curiel@famsa.com>
2019-11-06 14:58:28 -0600

    se modifi api para cargar imagenes

--- src/main/java/com/curiel/catalogos/controller/FileController.java
+++ src/main/java/com/curiel/catalogos/controller/FileController.java
@@ -1,14 +1,13 @@
 package com.curiel.catalogos.controller;
 
 import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
+import org.springframework.web.util.UriComponentsBuilder;
 import com.curiel.catalogos.service.FileService;
 
 @RestController
@@ -19,7 +18,11 @@ public class FileController {
      private FileService fileService;
      
     @PostMapping("/cargar")
-     public String cargarFile(@RequestParam("archivo") MultipartFile archivo) throws IOException {
+     public String cargarFile(@RequestParam Long campoid,@RequestParam("archivo") MultipartFile archivo,UriComponentsBuilder uriComponentsBuilder) throws IOException {
+          
+         if(archivo.isEmpty()) {
+             return "Archivo vacio";
+         }
          String nombreArvicvo=fileService.copiar(archivo);
          return nombreArvicvo;
      }
--- src/main/java/com/curiel/catalogos/service/FileService.java
+++ src/main/java/com/curiel/catalogos/service/FileService.java
@@ -14,16 +14,25 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileService {
     private final Logger log = LoggerFactory.getLogger(FileService.class);
 
-    private static final String  RUTA_ARCHIVO="files";
+    private static final String  RUTA_ARCHIVO="files/";
     
-    public String copiar(MultipartFile archivo) throws IOException {
-        String nombreArchivo = UUID.randomUUID().toString() + "_" +  archivo.getOriginalFilename().replace(" ", "");
+    /*public String copiar(MultipartFile archivo) throws IOException {
+        String nombreArchivo = UUID.randomUUID().toString() + "_" +archivo.getOriginalFilename().replace(" ", "");
         Path rutaArchivo = getPath(nombreArchivo);
         log.info(rutaArchivo.toString());
         Files.copy(archivo.getInputStream(), rutaArchivo);
         return nombreArchivo;
-    }
+    }*/
     public Path getPath(String nombreFoto) {
         return Paths.get(RUTA_ARCHIVO).resolve(nombreFoto).toAbsolutePath();
+    }
+    
+    public String copiar(MultipartFile archivo) throws IOException {
+        String nombreArchivo = UUID.randomUUID().toString() + "." +archivo.getContentType().split("/")[1];
+        Path path = Paths.get(RUTA_ARCHIVO+nombreArchivo);
+        byte[] bytes=archivo.getBytes();
+        Files.write(path, bytes);
+        log.info(nombreArchivo);
+        return nombreArchivo;
     }
 }
