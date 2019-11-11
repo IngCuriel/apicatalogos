2550edaa676eb33c8171c100ac01d1ea258637e6
Eleazar Curiel Monjaraz <eleazar-00@hotmail.com>
2019-11-11 16:45:42 -0600

    add configuracion cors global en el proyecto

---
+++ src/main/java/com/curiel/catalogos/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.curiel.catalogos.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * <h1>Configuración global de CORS</h1>
+ * clase para manejar la configuracion de cors
+ * @author Eleazar Curiel Monjaraz
+ *
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
+	  /**
+     * Todos los orígenes y métodos GET, HEAD y POST están permitidos
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+        .allowCredentials(true);
+    }
+}
