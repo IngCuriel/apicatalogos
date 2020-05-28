efc8364f4931a8564bb7d5868f3160609f73edad
Eleazar Curiel Monjaraz <eleazar-00@hotmail.com>
2020-05-28 11:21:38 -0500

    se crea servicio movimientos para la paginacion

--- src/main/java/com/curiel/catalogos/repository/MovimientoRepository.java
+++ src/main/java/com/curiel/catalogos/repository/MovimientoRepository.java
@@ -1,13 +1,15 @@
 package com.curiel.catalogos.repository;
 import java.util.List;
-import java.util.Map;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.curiel.catalogos.model.entity.Movimiento;
 
 public interface MovimientoRepository extends JpaRepository<Movimiento,Long>{
 
-    List<Movimiento> findByClienteProveedor(String clienteProveedor);
+    List<Movimiento> findByClienteProveedorOrderByIdDesc(String clienteProveedor);
     
+    Page<Movimiento> findAllByOrderByIdDesc(Pageable pageable);
     
 }
--- src/main/java/com/curiel/catalogos/repository/ProductoRepository.java
+++ src/main/java/com/curiel/catalogos/repository/ProductoRepository.java
@@ -1,4 +1,4 @@
-package com.curiel.catalogos.repository;
+ package com.curiel.catalogos.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
