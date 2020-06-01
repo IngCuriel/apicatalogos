c6fa3c34fa4a56b84fba57c05772501301b9035e
Eleazar Curiel Monjaraz <eleazar.curiel@famsa.com>
2020-05-31 22:28:04 -0500

    Se mejora service movimiento pago

--- src/main/java/com/curiel/catalogos/repository/MovimientoFormaPagoRepository.java
+++ src/main/java/com/curiel/catalogos/repository/MovimientoFormaPagoRepository.java
@@ -1,9 +1,15 @@
 package com.curiel.catalogos.repository;
 
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.curiel.catalogos.model.entity.Movimiento;
 import com.curiel.catalogos.model.entity.MovimientoFormaPago;
 
 public interface MovimientoFormaPagoRepository extends JpaRepository<MovimientoFormaPago,Long>{
 
+	List<MovimientoFormaPago> findByMovimiento(Movimiento movimiento);
+	
 }
--- src/main/java/com/curiel/catalogos/service/MovimientoFormaPagoService.java
+++ src/main/java/com/curiel/catalogos/service/MovimientoFormaPagoService.java
@@ -1,20 +1,41 @@
 package com.curiel.catalogos.service;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.curiel.catalogos.model.dto.MovimientoFormaPagoDto;
+import com.curiel.catalogos.model.entity.FormaDePago;
+import com.curiel.catalogos.model.entity.Movimiento;
 import com.curiel.catalogos.model.entity.MovimientoFormaPago;
+import com.curiel.catalogos.repository.MovimientoFormaPagoRepository;
 import com.curiel.catalogos.util.GenericService;
 
+@Service
 public class MovimientoFormaPagoService implements GenericService<MovimientoFormaPagoDto, MovimientoFormaPago, Long>{
-
-	@Override
+    
+	@Autowired
+    private MovimientoFormaPagoRepository movimientoFormaPagoRepository;
+	
+    @Override
+    @Transactional(readOnly=true)
 	public Set<MovimientoFormaPagoDto> list() {
-		// TODO Auto-generated method stub
-		return null;
+		Set<MovimientoFormaPagoDto> movimientoFPSet=new LinkedHashSet<>();
+		movimientoFormaPagoRepository.findAll().forEach(pagos->movimientoFPSet.add(convertToDto(pagos)));
+		return movimientoFPSet;
 	}
+    
+    @Transactional(readOnly=true)
+  	public Set<MovimientoFormaPagoDto> findByMovimientoId(Long id) {
+  		Set<MovimientoFormaPagoDto> movimientoFPSet=new LinkedHashSet<>();
+  		Movimiento movimiento=new Movimiento();
+  		movimiento.setId(id);
+  		movimientoFormaPagoRepository.findByMovimiento(movimiento).forEach(pagos->movimientoFPSet.add(convertToDto(pagos)));
+  		return movimientoFPSet;
+  	}
 
 	@Override
 	public void delete(Long id) {
@@ -37,8 +58,16 @@ public class MovimientoFormaPagoService implements GenericService<MovimientoForm
 
 	@Override
 	public MovimientoFormaPagoDto convertToDto(MovimientoFormaPago entity) {
-		// TODO Auto-generated method stub
-		return null;
+		MovimientoFormaPagoDto movimientoFormaPagoDto=new MovimientoFormaPagoDto();
+		movimientoFormaPagoDto.setId(entity.getId());
+		movimientoFormaPagoDto.setPago(entity.getPago());
+		movimientoFormaPagoDto.setDescripcionPago(entity.getDescripcionPago());
+		movimientoFormaPagoDto.setComprobantePago(entity.getComprobantePago());
+		FormaDePago formaDePago=new FormaDePago();
+		formaDePago.setNombre(entity.getFormaDePago().getNombre());
+		formaDePago.setId(entity.getFormaDePago().getId());
+		movimientoFormaPagoDto.setFormaDePago(formaDePago);
+		return movimientoFormaPagoDto;
 	}
 
 	@Override
